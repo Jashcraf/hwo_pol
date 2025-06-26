da45cec839998d6645073294a009bb9d2ce2f18b
Jashcraf <jarenashcraft@ucsb.edu>
2025-06-26 12:12:35 -1000

    progress on getting zernike decomposition

--- hwo_pol/zernike.py
+++ hwo_pol/zernike.py
@@ -1,3 +1,57 @@
 """
 Zernike analysis of Jones pupils
 """
+from prysm.mathops import np
+from prysm.coordinates import make_xy_grid, cart_to_polar
+from prysm.polynomials import noll_to_nm, zernike_nm_sequence, lstsq
+
+def zernike_coefficients_jones(jones_pupil, nmodes=37, mask=None):
+    """Generate Zernike coefficients from a Jones pupil
+
+    Parameters
+    ----------
+    jones_pupil : ndarray
+        Jones pupil of shape (ny, nx, 2, 2)
+    nmodes : int, optional
+        Number of noll-indexed modes to compute, by default 37
+    mask : ndarray, optional
+        binary mask to apply to the pupil, shape (nx, ny, 2, 2), by default None
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+
+    assert jones_pupil.ndim == 4, "Input must be a 4D array"
+    assert jones_pupil.shape[-2:] == (2, 2), "Input contain a 2x2 Jones matrix"
+
+    # construct a basis
+    x, y = make_xy_grid(jones_pupil.shape[:-2])
+    r, theta = cart_to_polar(x, y)
+
+    # Init zernike indices
+    nms = [noll_to_nm(i) for i in range(1, nmodes + 1)]
+
+    # Generate zernike basis
+    basis = list(zernike_nm_sequence(nms, r, theta))
+
+    coefficients = np.zeros([nmodes, 2, 2], dtype=np.complex128)
+
+    # lstsq to get coefficients, loop over Jones indices
+    for i in range(2):
+        for j in range(2):
+
+            jones_select = jones_pupil[..., i, j]
+
+            if mask is not None:
+                jones_select = jones_select / mask
+
+            coefficients[:, i, j] = lstsq(basis, jones_select)
+
+    return coefficients
+            
+
+
+
+
--- scripts/gen_jones_pupil.py
+++ scripts/gen_jones_pupil.py
@@ -1,20 +1,78 @@
-from hwo_pol.writing import read_model
 import poke.plotting as plot
 from poke.poke_math import np
+import ipdb
 
-models = ["EAC1", "EAC4", "EAC5"]
-wavelengths = [250, 550, 760, 950, 1500]
-bandwidths = [0.1, 0.2, 0.2, 0.2, 0.2]
+from hwo_pol.writing import read_model
+from hwo_pol.models import EAC1, EAC4, EAC5
+from hwo_pol.zernike import zernike_coefficients_jones
+
 N_LAMS = 5 # spectral sampling
+N_RAYS = 32
+ending = "TCA" # OTA or TCA
 
-for model_folder in models:
+# Set up EAC models
+models = [EAC1, EAC4, EAC5]
 
-    eac = read_model(model_folder)
+# Set up coatings to load
+coating_ota  = "XeLiF.json"
 
-    for wvl, bw in zip(wavelengths, bandwidths):
+coatings_internal = [
+    "EnhancedAg.json",
+    "ProtectedAg.json",
+    "ProtectedAg.json",
+    "ProtectedAg.json",
+    "ProtectedAg.json"
+]
 
-        wavelengths = np.linspace(wvl * bw / 2, wvl * bw / 2, N_LAMS)
+# Set up spectral data
+center_wavelengths = [350, 550, 760, 950, 1500]
+bandwidths = [0.1, 0.2, 0.2, 0.2, 0.2]
 
-        for w in wavelengths:
+# Set up Fields of View
+fov_1 = -0.07302
+fov_4 = -0.025
+fov_5 = -0.025
+fovs = [fov_1, fov_4, fov_5]
 
-            # re-generate surfaces
+# Init the EAC objects
+for EAC, fov in zip(models, fovs):
+    for wvl, bw, internal in zip(center_wavelengths, bandwidths, coatings_internal):
+        
+        # Set up bandpass
+        short_wvl = wvl * (1 - bw/2)
+        long_wvl = wvl * (1 + bw/2)
+        wavelengths = np.linspace(short_wvl, long_wvl, N_LAMS)
+
+        # init the EAC model
+        eac = EAC(N_RAYS, fov, wavelengths, coating_ota, internal, ending=ending)
+        
+        # Trace the rays
+        eac.trace_rays()
+
+        # Compute the Jones pupil
+        eac.compute_jones_pupil_by_wavelength()
+
+        # Compute the Zernike coefficients
+        for wvl in eac.wavelengths:
+            jones = eac.rayfronts[wvl].jones_pupil[-1][..., :2, :2]
+            jones = jones.reshape([N_RAYS, N_RAYS, 2, 2])
+
+            # Compute the Zernike coefficients
+            coeffs = zernike_coefficients_jones(jones)
+            
+            plt.figure()
+            plt.title("Imaginary Zernike Coefficients, "+r"$a + ib$") 
+            
+            # plot the Zernike coefficients
+            plt.plot(coeffs[:, 0, 0].real, label=r"$a_{xx}$")
+            plt.plot(coeffs[:, 0, 1].real, label=r"$a_{xy}$")
+            plt.plot(coeffs[:, 1, 0].real, label=r"$a_{yx}$")
+            plt.plot(coeffs[:, 1, 1].real, label=r"$a_{yy}$")
+
+            plt.plot(coeffs[:, 0, 0].imag, label=r"$\phi_{xx}$")
+            plt.plot(coeffs[:, 0, 1].imag, label=r"$\phi_{xy}$")
+            plt.plot(coeffs[:, 1, 0].imag, label=r"$\phi_{yx}$")
+            plt.plot(coeffs[:, 1, 1].imag, label=r"$\phi_{yy}$")
+
+            plt.legend()
+            plt.show()
